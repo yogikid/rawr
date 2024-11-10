67dec9a7e567cbd7e370f929d753a52f399d7b12
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-11-10 12:31:36 +0700

    add: offline page

---
+++ src/components/layouts/partials/Blank.jsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import Overlay from './Overlay';
+
+const Blank = ({ children, className = '', ...others }) => {
+  return (
+    
+    <div className={`main group/main pb-6 p-6 h-[10dvh] ${className}`} {...others}>
+      <section className='group-[.sidebar-expanded]/main:blur-[2px]  mx-auto max-w-[1024px]'>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default Blank;
--- src/pages/_offline.js
+++ src/pages/_offline.js
@@ -1,7 +1,55 @@
-const Fallback  = () => {
-  return (
-    <div>offline</div>
-  )
-}
+import { useTranslations } from 'next-intl';
+import { NextSeo } from 'next-seo';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Blank from '@/components/layouts/partials/Blank';
 
-export default Fallback 
+const Fallback = () => {
+  const t = useTranslations('Offline');
+  const router = useRouter();
+  const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${router.asPath}`;
+
+  const handleRetry = () => {
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+      </Head>
+      <NextSeo title={`${t('title')} - Dwi Wijaya`} />
+
+      <Blank className='text-center flex justify-center !items-center !h-[100dvh]'>
+        {/* Page Heading with Icon */}
+        <div className="flex flex-col gap-6">
+
+          <div className="">
+            <h2 className="mb-1 relative font-bold text-3xl text-title">{t('title')}</h2>
+            <p className="leading-5 text-subtext">
+              {t('subtitle')}
+            </p>
+          </div>
+
+          <i className="text-primary text-7xl fa-duotone fa-solid fa-wifi-exclamation"></i>
+
+          <p className="max-w-[26rem] mx-auto text-subtext text-lg">{t('message')}</p>
+
+          <div className="flex justify-center">
+            <button
+              onClick={handleRetry}
+              className="btn"
+            >
+              <i class="fa-duotone fa-solid fa-rotate-right"></i>
+              {t('retry')}
+            </button>
+          </div>
+        </div>
+
+      </Blank>
+    </>
+  );
+};
+
+export default Fallback;
