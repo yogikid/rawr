febe1696a8bb587386c51aa0ce6b25cb8de1b81c
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-11-11 23:44:02 +0700

    optimize: image component

--- src/components/elements/Image.jsx
+++ src/components/elements/Image.jsx
@@ -1,9 +1,7 @@
-import NextImage, { ImageProps as NextImageProps } from 'next/image';
+import NextImage from 'next/image';
 import { useState } from 'react';
 
-
-const Image = (props) => {
-  const { alt, src, className, rounded, ...rest } = props;
+const Image = ({ alt, src, className, rounded, ...rest }) => {
   const [isLoading, setLoading] = useState(true);
 
   return (
@@ -11,20 +9,17 @@ const Image = (props) => {
       className={`overflow-hidden ${isLoading ? 'animate-pulse' : ''} ${rounded} relative`}
     >
       <NextImage
-        className={`
-          duration-700 ease-in-out
-          ${className} ${isLoading
-            ? 'scale-[1.02] blur-xl grayscale'
-            : 'scale-100 blur-0 grayscale-0'} `}
+        className={`duration-700 ease-in-out ${className} ${isLoading ? 'scale-[1.02] blur-xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
         src={src}
         alt={alt}
-        loading='lazy'
-        // priority={true}
-        quality={100}
+        loading="lazy"
+        quality={75} 
+        sizes="(max-width: 768px) 100vw, 50vw"
         onLoadingComplete={() => setLoading(false)}
         {...rest}
       />
     </div>
   );
 };
+
 export default Image;
