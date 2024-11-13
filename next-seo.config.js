eec5ef49d9aaa2322edff71ab1a7ce28e45fd96b
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-11-13 08:57:25 +0700

    seo: optimize default seo config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,4 +1,4 @@
-const metaDescription = 'Dwi Wijaya is a Full-Stack Developer dedicated to creating efficient, scalable, and visually engaging web applications. Passionate about bringing digital visions to life with seamless user experiences.';
+const metaDescription = 'Dwi Wijaya is a Full-Stack Developer dedicated to creating efficient, scalable, and visually engaging web applications. Passionate about bringing digital visions to life with seamless user experiences (@dwiwijaya).';
 const metaSquareImage = 'https://zyhdjqkdvsbxnpngpvkc.supabase.co/storage/v1/object/public/seo/squre-ogimage.png'
 const metaImage = 'https://firebasestorage.googleapis.com/v0/b/portfolio-dwiwijaya.appspot.com/o/meta-image.jpg?alt=media&token=45e644b5-1655-40e7-83b6-dc73bf3ecfa6';
 
@@ -8,7 +8,15 @@ const defaultSEOConfig = {
   openGraph: {
     title: 'Dwi Wijaya - Personal Website',
     description: metaDescription,
-    type: 'website',
+    url: 'https://dwiwijaya.com/',
+    type: 'profile',
+    siteName: 'Dwi Wijaya - Personal Website',
+    locale: 'en_US',
+    profile: {
+      first_name: 'Dwi',
+      last_name: 'Wijaya',
+      username: 'dwiwijaya',
+    },
     images: [
       {
         url: metaSquareImage,
@@ -35,13 +43,28 @@ const defaultSEOConfig = {
         height: 900,
       },
     ],
-    site_name: 'dwiwijaya.com',
+    
   },
   twitter: {
     handle: '@DwiWijaya',
     site: '@dwiwijaya',
+    title: 'Dwi Wijaya - Personal Website',
     cardType: 'summary_large_image',
   },
+  additionalMetaTags: [
+    {
+      name: 'robots',
+      content: 'index, follow',
+    },
+    {
+      name: 'keywords',
+      content: 'Dwi Wijaya, Dwi, Wijaya , Full-Stack Developer, Web Developer, Portfolio, Personal Website'
+    },
+    {
+      name: 'author',
+      content: 'Dwi Wijaya',
+    },
+  ],
 };
 
 export default defaultSEOConfig;
