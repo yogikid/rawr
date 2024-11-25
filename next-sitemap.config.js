769bdf4d9107d64b763cc9778bc0b6b381a6df59
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-11-25 10:58:22 +0700

    seo: sitemap and hreflang metatags

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://dwiwijaya.com',
   generateRobotsTxt: true,
+  alternateRefs: [
+    {
+      href: 'https://dwiwijaya.com/en',
+      hreflang: 'en',
+    },
+    {
+      href: 'https://dwiwijaya.com/',
+      hreflang: 'id',
+    },
+  ],
 };
