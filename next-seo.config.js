f951cabe50b973ecdfcecfc283681fc9f0d073df
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-11-26 20:40:40 +0700

    seo: meta desc

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,4 +1,4 @@
-const metaDescription = 'Dwi Wijaya, Full-Stack Developer, specializes in creating innovative web applications with a focus on seamless user experiences and high performance.';
+const metaDescription = 'Dwi Wijaya is a Full-Stack Developer, specializes in building efficient, scalable web & apps with seamless user experiences. Let’s bring your vision to life!';
 const metaSquareImage = 'https://zyhdjqkdvsbxnpngpvkc.supabase.co/storage/v1/object/public/seo/squre-ogimage.png'
 const metaImage = 'https://firebasestorage.googleapis.com/v0/b/portfolio-dwiwijaya.appspot.com/o/meta-image.jpg?alt=media&token=45e644b5-1655-40e7-83b6-dc73bf3ecfa6';
 
@@ -23,6 +23,24 @@ const defaultSEOConfig = {
         alt: 'Dwi Wijaya - og:image',
         width: 600,
         height: 600,
+      },
+      {
+        url: metaImage,
+        alt: 'Dwi Wijaya - og:image',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: metaImage,
+        alt: 'Dwi Wijaya - og:image',
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: metaImage,
+        alt: 'Dwi Wijaya - og:image',
+        width: 1600,
+        height: 900,
       },
     ],
     
