5ac3936947ead3369c5fafb0992a35239b38e8b1
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-11-29 11:33:10 +0700

    seo: move schema markup to _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
+import { schemaMarkup } from "@/lib/schema-markup";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaMarkup) }}
+        />
         <script defer src="https://analytics.dwiwijaya.com/script.js" data-website-id="7e71a6f6-f863-400e-806f-1af37e845ad1"></script>
       </Head>
       <body>
