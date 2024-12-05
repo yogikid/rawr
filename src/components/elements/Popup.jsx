1712e49d5c20b298fdc4c9b19f15da88f8490216
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-12-05 11:00:01 +0700

    style: popup transition animation

--- src/components/elements/Popup.jsx
+++ src/components/elements/Popup.jsx
@@ -1,18 +1,21 @@
 // components/Popup.js
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Popup = ({ isVisible, children }) => {
     return (
-        isVisible && (
-            <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                className="w-[14rem] absolute top-full right-0 mt-2 bg-container shadow-lg border border-stroke rounded-lg p-4 z-10"
-            >
-                {children}
-            </motion.div>
-        )
+        <AnimatePresence>
+            {isVisible && (
+                <motion.div
+                    initial={{ opacity: 0, y: -10 }} // Initial state (invisible, slightly above)
+                    animate={{ opacity: 1, y: 0 }}    // Final state (fully visible, at normal position)
+                    exit={{ opacity: 0, y: -10 }}     // State when the popup is removed (invisible, slightly above)
+                    transition={{ duration: 0.2 }}    // Smooth transition duration
+                    className="w-[14rem] absolute top-full right-0 mt-2 bg-container shadow-lg border border-stroke rounded-lg p-4 z-10"
+                >
+                    {children}
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 };
 
