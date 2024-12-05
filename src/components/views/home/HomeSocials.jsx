6f0eaa2a816e0f00bca59203f31e5632c609c6c2
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-12-05 10:09:00 +0700

    style: fix layout home social

--- src/components/views/home/HomeSocials.jsx
+++ src/components/views/home/HomeSocials.jsx
@@ -5,17 +5,18 @@ const HomeSocials = () => {
     return (
         <ul className="flex gap-2 mt-6">
             {Socials.map((social, index) => (
-                <li key={index}>
+                <li key={index} className='w-full'>
                     <a
                         data-umami-event={`Social - ${social.eventName}`}
                         aria-label={`go to ${social.label}`}
                         href={social.link}
                         target='_blank'
                         rel="noopener noreferrer"
-                        className="w-full sm:w-fit justify-center !py-3 flex text-xl text-text hover:text-primary badge items-center group"
+                        className="w-full justify-center !py-3 flex text-text hover:text-primary badge items-center group"
                     >
                         {/* <i className={`group-hover:-rotate-[10deg] transition-3s fa fa-brands fa-${social.boxIcon}`}></i> */}
                         {social.reactIcon}
+                        <span className='hidden md:block'>{social.label}</span>
                     </a>
                 </li>
             ))}
