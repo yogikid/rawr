54108a3273ad07b66cc34868712e4eebd7fc7c6b
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-01-24 14:23:51 +0700

    fix: faq data answers

--- src/constants/data/faq.js
+++ src/constants/data/faq.js
@@ -2,19 +2,19 @@ export const FAQ_DATA = {
     en: [
         {
             question: "Do you provide post-launch support?",
-            answer: "Yes, we offer post-launch support to ensure everything runs smoothly. We provide maintenance, updates, and troubleshooting as part of our service to make sure your website remains up-to-date and performs optimally.",
+            answer: "Yes, I offer post-launch support to ensure everything runs smoothly. I provide maintenance, updates, and troubleshooting as part of my service to make sure your website remains up-to-date and performs optimally.",
         },
         {
             question: "What makes your services different from others?",
-            answer: "We focus on creating tailored solutions that not only meet your technical needs but also provide a seamless user experience. We prioritize communication and collaboration to ensure that your vision is fully realized.",
+            answer: "I focus on creating tailored solutions that not only meet your technical needs but also provide a seamless user experience. I prioritize communication and collaboration to ensure that your vision is fully realized.",
         },
         {
             question: "Do you offer custom designs or use templates?",
-            answer: "We offer fully custom designs tailored to your brand and vision. Our designs are unique and crafted to meet your specific needs. We don’t rely on templates, ensuring your website stands out from the crowd.",
+            answer: "I offer fully custom designs tailored to your brand and vision. My designs are unique and crafted to meet your specific needs. I don’t rely on templates, ensuring your website stands out from the crowd.",
         },
         {
             question: "What is the process of working with you?",
-            answer: "Our process involves initial consultation, designing, development, testing, and finally deployment. We ensure your satisfaction at every step.",
+            answer: "My process involves initial consultation, designing, development, testing, and finally deployment. I ensure your satisfaction at every step.",
         },
         {
             question: "How long does it take to complete a project?",
@@ -24,19 +24,19 @@ export const FAQ_DATA = {
     id: [
         {
             question: "Apakah Anda menyediakan dukungan setelah peluncuran?",
-            answer: "Ya, kami menyediakan dukungan setelah peluncuran untuk memastikan semuanya berjalan lancar. Kami menawarkan pemeliharaan, pembaruan, dan pemecahan masalah sebagai bagian dari layanan kami agar situs web Anda tetap terkini dan berkinerja optimal.",
+            answer: "Ya, saya menyediakan dukungan setelah peluncuran untuk memastikan semuanya berjalan lancar. Saya menawarkan pemeliharaan, pembaruan, dan pemecahan masalah sebagai bagian dari layanan saya agar situs web Anda tetap terkini dan berkinerja optimal.",
         },
         {
             question: "Apa yang membuat layanan Anda berbeda dari yang lain?",
-            answer: "Kami fokus pada solusi yang disesuaikan untuk memenuhi kebutuhan teknis Anda sekaligus memberikan pengalaman pengguna yang mulus. Kami mengutamakan komunikasi dan kolaborasi untuk memastikan visi Anda terwujud sepenuhnya.",
+            answer: "Saya fokus pada solusi yang disesuaikan untuk memenuhi kebutuhan teknis Anda sekaligus memberikan pengalaman pengguna yang mulus. Saya mengutamakan komunikasi dan kolaborasi untuk memastikan visi Anda terwujud sepenuhnya.",
         },
         {
             question: "Apakah Anda menawarkan desain khusus atau menggunakan template?",
-            answer: "Kami menawarkan desain khusus yang sepenuhnya disesuaikan dengan merek dan visi Anda. Desain kami unik dan dibuat untuk memenuhi kebutuhan spesifik Anda. Kami tidak mengandalkan template, sehingga situs web Anda terlihat menonjol.",
+            answer: "Saya menawarkan desain khusus yang sepenuhnya disesuaikan dengan merek dan visi Anda. Desain saya unik dan dibuat untuk memenuhi kebutuhan spesifik Anda. Saya tidak mengandalkan template, sehingga situs web Anda terlihat menonjol.",
         },
         {
             question: "Bagaimana proses bekerja dengan Anda?",
-            answer: "Proses kami melibatkan konsultasi awal, desain, pengembangan, pengujian, dan akhirnya peluncuran. Kami memastikan kepuasan Anda di setiap langkah.",
+            answer: "Proses saya melibatkan konsultasi awal, desain, pengembangan, pengujian, dan akhirnya peluncuran. Saya memastikan kepuasan Anda di setiap langkah.",
         },
         {
             question: "Berapa lama waktu yang dibutuhkan untuk menyelesaikan proyek?",
@@ -44,4 +44,3 @@ export const FAQ_DATA = {
         },
     ],
 };
-
