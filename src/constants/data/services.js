d7d9b24c4bd2679763339af2270cc9d07b93651f
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2025-01-27 14:17:18 +0700

    chore: update service descriptions for clarity and consistency

--- src/constants/data/services.js
+++ src/constants/data/services.js
@@ -1,50 +1,50 @@
 export const ServicesData = [
-    {
-      id: 2,
-      icon: "browser",
-      title: {
-        en: "Web Development",
-        id: "Pengembangan Web",
-      },
-      description: {
-        en: "Build dynamic and responsive websites tailored to your needs, ensuring seamless functionality and optimal performance.",
-        id: "Membangun situs web yang dinamis dan responsif sesuai dengan kebutuhan Anda. Kami pastikan semuanya berfungsi dengan mulus dan performa optimal.",
-      }
+  {
+    id: 2,
+    icon: "browser",
+    title: {
+      en: "Web Development",
+      id: "Pengembangan Web",
     },
-    {
-      id: 1,
-      icon: "palette",
-      title: {
-        en: "UI/UX design",
-        id: "Desain UI/UX",
-      },
-      description: {
-        en: "Craft visually stunning and user-friendly interfaces that captivate your audience and enhance user experience.",
-        id: "Menciptakan antarmuka yang memukau secara visual dan mudah digunakan. Kami berfokus pada pengalaman pengguna yang memikat dan intuitif."
-      }
+    description: {
+      en: "I create dynamic, responsive websites that work seamlessly and perform at their best.",
+      id: "Saya membuat situs web dinamis dan responsif yang berfungsi dengan mulus dan memiliki performa terbaik.",
+    }
+  },
+  {
+    id: 1,
+    icon: "palette",
+    title: {
+      en: "UI/UX design",
+      id: "Desain UI/UX",
     },
-    {
-      id: 3,
-      icon: "pen-nib",
-      title: {
-        en: "Design",
-        id: "Desain",
-      },
-      description: {
-        en: "Create captivating visuals and branding materials that leave a lasting impression on your audience.",
-        id: "Membuat desain visual dan materi branding yang menarik dan meninggalkan kesan mendalam pada audiens Anda.",
-      }
+    description: {
+      en: "I design stunning, intuitive interfaces that captivate users and elevate their experience.",
+      id: "Saya mendesain antarmuka yang memukau dan intuitif, meningkatkan pengalaman pengguna secara maksimal.",
+    }
+  },
+  {
+    id: 3,
+    icon: "pen-nib",
+    title: {
+      en: "Design",
+      id: "Desain",
     },
-    {
-      id: 4,
-      icon: "messages-question",
-      title: {
-        en: "Consulting",
-        id: "Konsultasi",
-      },
-      description: {
-        en: "Discuss any other specific needs or services you require, let's tailor a solution to meet your unique requirements.",
-        id: "Diskusikan kebutuhan khusus atau layanan lainnya yang Anda butuhkan. Mari kita sesuaikan solusi untuk memenuhi kebutuhan unik Anda."
-      }
+    description: {
+      en: "I create impactful visuals and branding that leave a lasting impression on your audience.",
+      id: "Saya menciptakan desain visual dan branding yang berkesan dan meninggalkan dampak kuat pada audiens.",
+    }
+  },
+  {
+    id: 4,
+    icon: "messages-question",
+    title: {
+      en: "Consulting",
+      id: "Konsultasi",
     },
-  ];
+    description: {
+      en: "Let’s discuss your unique needs and find the perfect solution tailored just for you.",
+      id: "Mari kita diskusikan kebutuhan khusus Anda dan temukan solusi terbaik yang saya dirancang untuk Anda.",
+    }
+  },
+];
