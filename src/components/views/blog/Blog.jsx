7a8ba4ac7eba4e016792673a2dd885d4971a4407
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2025-01-28 20:09:38 +0700

    style: comingsoon icon color

--- src/components/views/blog/Blog.jsx
+++ src/components/views/blog/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
     return (
         <>
             <div className="blog__container">
-                <h3 className='flex gap-2 items-center'><i className='fad fa-clock text-primary'></i> {t('Common.comingsoon')}</h3>
+                <h3 className='flex gap-2 items-center'><i className='fad fa-clock'></i> {t('Common.comingsoon')}</h3>
             </div>
         </>
     )
