c0f73664a300d85144bce3ae1383b3e453f43bce
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-04-24 10:04:24 +0700

    fix: dashboard pagespeed api key

--- src/constants/pagespeed.js
+++ src/constants/pagespeed.js
@@ -1,4 +1,4 @@
 export const PAGESPEED_URL = 'https://pagespeed.web.dev/'
 export const PAGESPEED_BASE = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=performance&category=best-practices&category=seo'
-export const PAGESPEED_API = PAGESPEED_BASE + '&url=' + process.env.NEXT_PUBLIC_SITE_URL
+export const PAGESPEED_API = PAGESPEED_BASE + '&key=' + process.env.NEXT_PUBLIC_GOOGLE_PAGESPEED_KEY + '&url=' + process.env.NEXT_PUBLIC_SITE_URL 
 
