cd43d18453b2214a313a1efe650b97d6b088124f
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2025-04-27 12:38:33 +0700

    style: resize social icons size

--- src/constants/data/socials.js
+++ src/constants/data/socials.js
@@ -3,35 +3,35 @@ import { MdOutlineAlternateEmail } from 'react-icons/md'
 
 export const Socials = [
     {
-        reactIcon : <MdOutlineAlternateEmail/>,
-        background: '#16a34c',
+        reactIcon : <MdOutlineAlternateEmail size={18}/>,
+        background: '#18a34c',
         link: 'https://mailto:hello@dwiwijaya.com',
         eventName: 'Contact: Email',
         label: 'Email',
     },
     {
-        reactIcon : <RiInstagramFill size={16}/>,
+        reactIcon : <RiInstagramFill size={18}/>,
         background: '#c2410c',
         link: 'https://www.instagram.com/_dwiwijaya_/',
         eventName: 'Social: Instagram',
         label: 'Instagram',
     },
     {
-        reactIcon : <RiLinkedinFill size={16}/>,
+        reactIcon : <RiLinkedinFill size={18}/>,
         background: '#3b82f6',
         link: 'https://www.linkedin.com/in/dwiwijaya/',
         eventName: 'Social: Linkedin',
         label: 'Linkedin',
     },
     {
-        reactIcon : <RiGithubFill size={16}/>,
+        reactIcon : <RiGithubFill size={18}/>,
         background: '#000',
         link: 'https://github.com/dwiwijaya',
         eventName: 'Social: Github',
         label: 'Github',
     },
     {
-        reactIcon : <RiTelegram2Fill size={16}/>,
+        reactIcon : <RiTelegram2Fill size={18}/>,
         background: '#0088cc',
         link: 'https://t.me/dwiiwijaya',
         eventName: 'Contact: Telegram',
