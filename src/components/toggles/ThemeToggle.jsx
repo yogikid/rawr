23f6d09b2476280545ecaf8d489b396de971186e
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-04-29 10:46:57 +0700

    style: fix glitch theme toggle

--- src/components/toggles/ThemeToggle.jsx
+++ src/components/toggles/ThemeToggle.jsx
@@ -31,14 +31,14 @@ const ThemeToggle = () => {
                     onChange={toggleTheme}
                 />
                 <div className="slider bg-background peer-checked:bg-background grid grid-cols-2 rounded-2xl justify-between items-center w-full h-full relative">
-                    <span
-                        className={`absolute w-1/2 h-full border border-stroke rounded-xl transform ${mounted && 'bg-container'} ${currentTheme === "dark" ? 'right-0 ' : 'left-0 '}`}
+                    {mounted && <span
+                        className={`absolute h-full border border-stroke rounded-xl bg-container ${theme === "dark" && 'right-0 w-1/2 '} ${theme === "light" && 'w-1/2 left-0'}`}
                         style={{ content: '""', zIndex: 1 }}
-                    />
-                    <p className={`z-10 p-3 justify-center leading-4 flex items-center gap-2 text-slate-500 ${currentTheme === 'light' ? '!text-slate-800' : ''}`}>
+                    />}
+                    <p className={`z-10 p-3 justify-center leading-4 flex items-center gap-2 text-slate-500 ${currentTheme === 'light' && '!text-slate-800'}`}>
                         <i className="fad fa-sun-bright"></i>{t("Sidebar.light")}
                     </p>
-                    <p className={`z-10 p-3 justify-center leading-4 flex items-center gap-2 text-slate-500 ${currentTheme === 'dark' ? '!text-slate-300' : ''}`}>
+                    <p className={`z-10 p-3 justify-center leading-4 flex items-center gap-2 text-slate-500 ${currentTheme === 'dark' && '!text-slate-300'}`}>
                         <i className="fad fa-moon"></i>{t("Sidebar.dark")}
                     </p>
                 </div>
