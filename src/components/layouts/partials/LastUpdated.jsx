6f783324be71bc10c2305d44a471df8936f5caa7
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-04-29 10:55:45 +0700

    style: fix sidebar fontsize lastupdate

--- src/components/layouts/partials/LastUpdated.jsx
+++ src/components/layouts/partials/LastUpdated.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const LastUpdated = ({ t, lastUpdate }) => {
     return (
         <div className="text-left mt-4 sm:mt-8">
-            <h2 className='text-2xl mb-2 leading-6 font-se'>{t('Sidebar.welcome')}</h2>
-            <time className="text-sm text-subtext">
+            <h2 className='text-2xl mb-2 leading-6'>{t('Sidebar.welcome')}</h2>
+            <time className="text-[.8rem] text-subtext">
                 {t("Sidebar.lastupdate")} : {lastUpdate}
             </time>
         </div>
