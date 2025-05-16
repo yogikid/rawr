bd66de8750ea88ec2f48ebc719cc2233657b8c5e
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2025-05-16 11:35:19 +0700

    fix: sidebar add icon for footer and last updated

--- src/components/layouts/partials/Footer.jsx
+++ src/components/layouts/partials/Footer.jsx
@@ -1,7 +1,15 @@
+import { PiCopyright } from "react-icons/pi"
+
 const Footer = () => {
     return (
-        <footer className="nav__footer text-left pl-6">
-            <span className="text-subtext text-sm transform rotate-180 ">&copy; {new Date().getFullYear()} Dwi-wijaya</span>
+        <footer className="nav__footer text-left pl-6 flex flex-col gap-1">
+            <span className="text-subtext text-sm flex items-center gap-1 whitespace-nowrap">
+                <PiCopyright size={15} className="text-primary" />
+                {new Date().getFullYear()} Dwi wijaya
+            </span>
+            <span className="text-subtext text-xs flex items-center gap-1">
+                Open Source under GPL License
+            </span>
         </footer>
     )
 }
--- src/components/layouts/partials/LastUpdated.jsx
+++ src/components/layouts/partials/LastUpdated.jsx
@@ -1,9 +1,11 @@
+import { PiArrowsClockwise } from "react-icons/pi"
 const LastUpdated = ({ t, lastUpdate }) => {
     return (
         <div className="text-left mt-4 sm:mt-8">
             <h2 className='text-2xl mb-2 leading-6'>{t('Sidebar.welcome')}</h2>
-            <time className="text-[.8rem] text-subtext">
+            <time className="text-[.8rem] text-subtext flex items-center gap-1">
                 {t("Sidebar.lastupdate")} : {lastUpdate}
+                <PiArrowsClockwise  className="text-primary"/> 
             </time>
         </div>
     )
